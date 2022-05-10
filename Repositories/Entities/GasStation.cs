aaff121a513f840fe9f92e505e8dc4358c20feb4
Cucerenco Maxim <maxim.cucerenco@codefactorygroup.com>
2022-05-10 18:18:58 +0300

    Swagger works perfect

--- Repositories/Entities/GasStation.cs
+++ Repositories/Entities/GasStation.cs
@@ -1,3 +1,5 @@
+using Mapster;
+
 namespace Repositories.Entities;
 
 public class GasStation
@@ -12,13 +14,12 @@ public class GasStation
     public bool Market { get; set; }
     public bool Coffee { get; set; }
     public bool CarWash { get; set; }
-    
+
     public int CompanyId { get; set; }
     public int? CoffeeShopId { get; set; }
     public int? CarWashId { get; set; }
-    
+
     public Company Company { get; set; }
-    public CoffeeShop? CoffeeShop { get; set; }
+    [AdaptIgnore] public CoffeeShop? CoffeeShop { get; set; }
     public CarWash? CarWashData { get; set; }
-    
 }
